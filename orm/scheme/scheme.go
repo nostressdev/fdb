adf8f089824cfef3b9b820e382adc308c913e73e
levtul <61029074+levtul@users.noreply.github.com>
2022-05-15 13:01:19 +0300

    ORM Generator Description Scheme (#1)
    * basic models for generator scheme description

---
+++ orm/scheme/scheme.go
@@ -0,0 +1,39 @@
+package fdb
+
+type GeneratorConfig struct {
+	Models []Model
+	Tables []Table
+}
+
+type Model struct {
+	Name          string
+	Fields        map[string]Field
+	ExternalModel string // продумать как хранить инфу о протобафе, может что то дополнительное
+}
+
+type Field struct {
+	Name         string
+	Type         string
+	DefaultValue interface{}
+}
+
+type Table struct {
+	Name         string
+	StoragePath  string
+	RangeIndexes []RangeIndex
+	Columns      map[string]Column
+	PK           []string // column names
+}
+
+type Column struct {
+	Name         string
+	Type         string
+	DefaultValue interface{}
+}
+
+type RangeIndex struct {
+	Name    string
+	IK      []string // column names
+	Columns []string
+	Async   bool
+}
