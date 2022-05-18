8e644a109faaa334320edbf946b106e553e49687
lev <wot.k@mail.ru>
2022-05-18 20:13:45 +0000

    cleanup code & change errors

--- orm/scheme/scheme.go
+++ orm/scheme/scheme.go
@@ -1,6 +1,8 @@
 package scheme
 
-import "fmt"
+import (
+	"github.com/nostressdev/fdb/orm/scheme/errors"
+)
 
 type GeneratorConfig struct {
 	Models []Model `yaml:"models"`
@@ -40,11 +42,11 @@ type RangeIndex struct {
 	Async   bool     `yaml:"async"`
 }
 
-func (c *GeneratorConfig) Validate() error {
+func (c *GeneratorConfig) validateModels() error {
 	modelsSet := make(map[string]struct{})
 	for _, model := range c.Models {
 		if _, ok := modelsSet[model.Name]; ok {
-			return fmt.Errorf("model %s is duplicated", model.Name)
+			return errors.ValidationError.Newf("model %s is duplicated", model.Name)
 		}
 		modelsSet[model.Name] = struct{}{}
 		if model.ExternalModel != "" {
@@ -53,75 +55,100 @@ func (c *GeneratorConfig) Validate() error {
 		set := make(map[string]struct{})
 		for _, field := range model.Fields {
 			if field.Type == "" {
-				return fmt.Errorf("field %s:%s has no type", model.Name, field.Name)
+				return errors.ValidationError.Newf("field %s:%s has no type", model.Name, field.Name)
 			}
 			if _, ok := set[field.Name]; ok {
-				return fmt.Errorf("field %s:%s is duplicated", model.Name, field.Name)
+				return errors.ValidationError.Newf("field %s:%s is duplicated", model.Name, field.Name)
 			}
 			set[field.Name] = struct{}{}
 		}
-
 	}
-	// validate tables
+	return nil
+}
+
+func (c *GeneratorConfig) validateColumns(table *Table) (map[string]struct{}, error) {
+	columnsSet := make(map[string]struct{})
+	for _, column := range table.Columns {
+		if _, ok := columnsSet[column.Name]; ok {
+			return nil, errors.ValidationError.Newf("column %s:%s is duplicated", table.Name, column.Name)
+		}
+		columnsSet[column.Name] = struct{}{}
+		if column.Name == "" {
+			return nil, errors.ValidationError.Newf("table %s: column has no name", table.Name)
+		}
+		if column.Type == "" {
+			return nil, errors.ValidationError.Newf("table %s: column %s has no type", table.Name, column.Name)
+		}
+	}
+	for _, pk := range table.PK {
+		if _, ok := columnsSet[pk]; !ok {
+			return nil, errors.ValidationError.Newf("table %s: primary key %s is not in columns", table.Name, pk)
+		}
+	}
+	return columnsSet, nil
+}
+
+func (c *GeneratorConfig) validateIndexes(table *Table, columnsSet map[string]struct{}) error {
+	indexesSet := make(map[string]struct{})
+	for _, index := range table.RangeIndexes {
+		if _, ok := indexesSet[index.Name]; ok {
+			return errors.ValidationError.Newf("table %s: range index %s is duplicated", table.Name, index.Name)
+		}
+		if index.Name == "" {
+			return errors.ValidationError.Newf("table %s: range index has no name", table.Name)
+		}
+		if len(index.IK) == 0 {
+			return errors.ValidationError.Newf("table %s: range index %s has no ik", table.Name, index.Name)
+		}
+		if len(index.Columns) == 0 {
+			return errors.ValidationError.Newf("table %s: range index %s has no columns", table.Name, index.Name)
+		}
+		for _, ik := range index.IK {
+			if _, ok := columnsSet[ik]; !ok {
+				return errors.ValidationError.Newf("table %s: range index %s: ik %s is not in columns", table.Name, index.Name, ik)
+			}
+		}
+		for _, column := range index.Columns {
+			if _, ok := columnsSet[column]; !ok {
+				return errors.ValidationError.Newf("table %s: range index %s: column %s is not in columns", table.Name, index.Name, column)
+			}
+		}
+	}
+	return nil
+}
+
+func (c *GeneratorConfig) validateTables() error {
 	tablesSet := make(map[string]struct{})
 	for _, table := range c.Tables {
 		if _, ok := tablesSet[table.Name]; ok {
-			return fmt.Errorf("table %s is duplicated", table.Name)
+			return errors.ValidationError.Newf("table %s is duplicated", table.Name)
 		}
 		tablesSet[table.Name] = struct{}{}
 		if table.StoragePath == "" {
-			return fmt.Errorf("table %s has no storage path", table.Name)
+			return errors.ValidationError.Newf("table %s has no storage path", table.Name)
 		}
 		if len(table.Columns) == 0 {
-			return fmt.Errorf("table %s has no columns", table.Name)
+			return errors.ValidationError.Newf("table %s has no columns", table.Name)
 		}
 		if len(table.PK) == 0 {
-			return fmt.Errorf("table %s has no primary key", table.Name)
+			return errors.ValidationError.Newf("table %s has no primary key", table.Name)
 		}
-		columnsSet := make(map[string]struct{})
-		for _, column := range table.Columns {
-			if _, ok := columnsSet[column.Name]; ok {
-				return fmt.Errorf("column %s:%s is duplicated", table.Name, column.Name)
-			}
-			columnsSet[column.Name] = struct{}{}
-			if column.Name == "" {
-				return fmt.Errorf("table %s: column has no name", table.Name)
-			}
-			if column.Type == "" {
-				return fmt.Errorf("table %s: column %s has no type", table.Name, column.Name)
-			}
+		columnsSet, err := c.validateColumns(&table)
+		if err != nil {
+			return err
 		}
-		for _, pk := range table.PK {
-			if _, ok := columnsSet[pk]; !ok {
-				return fmt.Errorf("table %s: primary key %s is not in columns", table.Name, pk)
-			}
-		}
-		indexesSet := make(map[string]struct{})
-		for _, index := range table.RangeIndexes {
-			if _, ok := indexesSet[index.Name]; ok {
-				return fmt.Errorf("table %s: range index %s is duplicated", table.Name, index.Name)
-			}
-			if index.Name == "" {
-				return fmt.Errorf("table %s: range index has no name", table.Name)
-			}
-			if len(index.IK) == 0 {
-				return fmt.Errorf("table %s: range index %s has no ik", table.Name, index.Name)
-			}
-			if len(index.Columns) == 0 {
-				return fmt.Errorf("table %s: range index %s has no columns", table.Name, index.Name)
-			}
-			for _, ik := range index.IK {
-				if _, ok := columnsSet[ik]; !ok {
-					return fmt.Errorf("table %s: range index %s: ik %s is not in columns", table.Name, index.Name, ik)
-				}
-			}
-			for _, column := range index.Columns {
-				if _, ok := columnsSet[column]; !ok {
-					return fmt.Errorf("table %s: range index %s: column %s is not in columns", table.Name, index.Name, column)
-				}
-			}
+		err = c.validateIndexes(&table, columnsSet)
+		if err != nil {
+			return err
 		}
 	}
-
 	return nil
+}
+
+func (c *GeneratorConfig) Validate() error {
+	err := c.validateModels()
+	if err != nil {
+		return err
+	}
+	return c.validateTables()
 }
