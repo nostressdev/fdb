9f4257929dde3267a5abdca68c2b7c9c5a905192
lev <wot.k@mail.ru>
2022-05-18 19:25:44 +0000

    simple parsing

--- orm/scheme/scheme.go
+++ orm/scheme/scheme.go
@@ -1,5 +1,7 @@
 package scheme
 
+import "fmt"
+
 type GeneratorConfig struct {
 	Models []Model `yaml:"models"`
 	Tables []Table `yaml:"tables"`
@@ -36,4 +38,90 @@ type RangeIndex struct {
 	IK      []string `yaml:"ik"`
 	Columns []string `yaml:"columns"`
 	Async   bool     `yaml:"async"`
+}
+
+func (c *GeneratorConfig) Validate() error {
+	modelsSet := make(map[string]struct{})
+	for _, model := range c.Models {
+		if _, ok := modelsSet[model.Name]; ok {
+			return fmt.Errorf("model %s is duplicated", model.Name)
+		}
+		modelsSet[model.Name] = struct{}{}
+		if model.ExternalModel != "" {
+			continue
+		}
+		set := make(map[string]struct{})
+		for _, field := range model.Fields {
+			if field.Type == "" {
+				return fmt.Errorf("field %s:%s has no type", model.Name, field.Name)
+			}
+			if _, ok := set[field.Name]; ok {
+				return fmt.Errorf("field %s:%s is duplicated", model.Name, field.Name)
+			}
+			set[field.Name] = struct{}{}
+		}
+
+	}
+	// validate tables
+	tablesSet := make(map[string]struct{})
+	for _, table := range c.Tables {
+		if _, ok := tablesSet[table.Name]; ok {
+			return fmt.Errorf("table %s is duplicated", table.Name)
+		}
+		tablesSet[table.Name] = struct{}{}
+		if table.StoragePath == "" {
+			return fmt.Errorf("table %s has no storage path", table.Name)
+		}
+		if len(table.Columns) == 0 {
+			return fmt.Errorf("table %s has no columns", table.Name)
+		}
+		if len(table.PK) == 0 {
+			return fmt.Errorf("table %s has no primary key", table.Name)
+		}
+		columnsSet := make(map[string]struct{})
+		for _, column := range table.Columns {
+			if _, ok := columnsSet[column.Name]; ok {
+				return fmt.Errorf("column %s:%s is duplicated", table.Name, column.Name)
+			}
+			columnsSet[column.Name] = struct{}{}
+			if column.Name == "" {
+				return fmt.Errorf("table %s: column has no name", table.Name)
+			}
+			if column.Type == "" {
+				return fmt.Errorf("table %s: column %s has no type", table.Name, column.Name)
+			}
+		}
+		for _, pk := range table.PK {
+			if _, ok := columnsSet[pk]; !ok {
+				return fmt.Errorf("table %s: primary key %s is not in columns", table.Name, pk)
+			}
+		}
+		indexesSet := make(map[string]struct{})
+		for _, index := range table.RangeIndexes {
+			if _, ok := indexesSet[index.Name]; ok {
+				return fmt.Errorf("table %s: range index %s is duplicated", table.Name, index.Name)
+			}
+			if index.Name == "" {
+				return fmt.Errorf("table %s: range index has no name", table.Name)
+			}
+			if len(index.IK) == 0 {
+				return fmt.Errorf("table %s: range index %s has no ik", table.Name, index.Name)
+			}
+			if len(index.Columns) == 0 {
+				return fmt.Errorf("table %s: range index %s has no columns", table.Name, index.Name)
+			}
+			for _, ik := range index.IK {
+				if _, ok := columnsSet[ik]; !ok {
+					return fmt.Errorf("table %s: range index %s: ik %s is not in columns", table.Name, index.Name, ik)
+				}
+			}
+			for _, column := range index.Columns {
+				if _, ok := columnsSet[column]; !ok {
+					return fmt.Errorf("table %s: range index %s: column %s is not in columns", table.Name, index.Name, column)
+				}
+			}
+		}
+	}
+
+	return nil
 }
--- orm/yaml/parse.go
+++ orm/yaml/parse.go
@@ -14,36 +14,70 @@ type Parser struct {
 	Models map[string]scheme.Model
 }
 
-func (p *Parser) parseField(value interface{}, field scheme.Field) (interface{}, error) {
-	switch field.Type {
+func (p *Parser) parseField(value interface{}, fieldType string) (interface{}, error) {
+
+	switch fieldType {
 	case "int32":
-		return value.(int32), nil
+		return int32(value.(int)), nil
 	case "int64":
-		return value.(int64), nil
+		return int64(value.(int)), nil
 	case "uint32":
-		return value.(uint32), nil
+		return uint32(value.(int)), nil
 	case "uint64":
-		return value.(uint64), nil
+		return uint64(value.(int)), nil
 	case "string":
 		return value.(string), nil
 	case "bool":
 		return value.(bool), nil
 	case "float":
-		return value.(float32), nil
+		return float32(value.(float64)), nil
 	case "double":
 		return value.(float64), nil
 	}
-	if strings.HasPrefix(field.Type, "@") {
-		typeName := field.Type[1:]
+	if strings.HasPrefix(fieldType, "@") {
+		typeName := fieldType[1:]
 		if model, ok := p.Models[typeName]; ok {
 			return p.parseModel(value, model)
 		}
 	}
-	return nil, fmt.Errorf("unknown type %s", field.Type)
+	return nil, fmt.Errorf("unknown type %s", fieldType)
 }
 
 func (p *Parser) parseModel(value interface{}, model scheme.Model) (interface{}, error) {
-	return nil, nil
+	var fieldsMap map[string]interface{}
+	var ok bool
+	if fieldsMap, ok = value.(map[string]interface{}); !ok {
+		return nil, fmt.Errorf("model %s must be a map", model.Name)
+	}
+	modelFieldNames := make(map[string]int)
+	for i, field := range model.Fields {
+		modelFieldNames[field.Name] = i
+	}
+	for name, value := range fieldsMap {
+		if _, ok := modelFieldNames[name]; !ok {
+			return nil, fmt.Errorf("model %s: field %s is not defined", model.Name, name)
+		}
+		value, err := p.parseField(value, model.Fields[modelFieldNames[name]].Type)
+		if err != nil {
+			return nil, err
+		}
+		fieldsMap[name] = value
+	}
+	return fieldsMap, nil
+}
+
+func (p *Parser) initModels(models []scheme.Model) error {
+	p.Models = make(map[string]scheme.Model)
+	for _, model := range models {
+		if model.ExternalModel != "" {
+			continue
+		}
+		if _, ok := p.Models[model.Name]; ok {
+			return fmt.Errorf("model %s is duplicated", model.Name)
+		}
+		p.Models[model.Name] = model
+	}
+	return nil
 }
 
 func (p *Parser) parseConfig(text string) (*scheme.GeneratorConfig, error) {
@@ -51,6 +85,30 @@ func (p *Parser) parseConfig(text string) (*scheme.GeneratorConfig, error) {
 	err := yaml.Unmarshal([]byte(text), config)
 	if err != nil {
 		return nil, err
+	}
+	if err := config.Validate(); err != nil {
+		return nil, err
+	}
+	p.initModels(config.Models)
+	for _, model := range config.Models {
+		for _, field := range model.Fields {
+			if field.DefaultValue != nil {
+				field.DefaultValue, err = p.parseField(field.DefaultValue, field.Type)
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
+	}
+	for _, table := range config.Tables {
+		for _, column := range table.Columns {
+			if column.DefaultValue != nil {
+				column.DefaultValue, err = p.parseField(column.DefaultValue, column.Type)
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
 	}
 	return config, nil
 }
