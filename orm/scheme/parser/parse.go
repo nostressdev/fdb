8bafd83debdc1e4c7477ce782776a36758cfa392
lev <wot.k@mail.ru>
2022-05-20 09:06:13 +0000

    add panics & cleanup

--- orm/scheme/parser/parse.go
+++ orm/scheme/parser/parse.go
@@ -3,98 +3,93 @@ package parser
 import (
 	"strings"
 
+	"github.com/nostressdev/fdb/errors"
 	"github.com/nostressdev/fdb/orm/scheme"
-	"github.com/nostressdev/fdb/orm/scheme/errors"
 )
 
 type ValuesParser struct {
 	Models map[string]scheme.Model
 }
 
-func (p *ValuesParser) parseField(value interface{}, fieldType string) (interface{}, error) {
+func (p *ValuesParser) parseField(value interface{}, fieldType string) interface{} {
+	if value == nil {
+		return nil
+	}
 	switch fieldType {
 	case "int32":
-		return int32(value.(int)), nil
+		return int32(value.(int))
 	case "int64":
-		return int64(value.(int)), nil
+		return int64(value.(int))
 	case "uint32":
-		return uint32(value.(int)), nil
+		return uint32(value.(int))
 	case "uint64":
-		return uint64(value.(int)), nil
+		return uint64(value.(int))
 	case "string":
-		return value.(string), nil
+		return value.(string)
 	case "bool":
-		return value.(bool), nil
+		return value.(bool)
 	case "float":
-		return float32(value.(float64)), nil
+		return float32(value.(float64))
 	case "double":
-		return value.(float64), nil
+		return value.(float64)
 	}
 	if strings.HasPrefix(fieldType, "@") {
 		typeName := fieldType[1:]
 		if model, ok := p.Models[typeName]; ok {
-			return p.parseModel(value, model)
+			if newValue, ok := value.(map[string]interface{}); ok {
+				return p.parseModel(newValue, model)
+			}
+			panic(errors.ParsingError.Newf("model %s: field %s is not a map", typeName, value))
 		}
 	}
-	return nil, errors.ParsingError.Newf("unknown type %s", fieldType)
+	panic(errors.ParsingError.Newf("unknown type %s", fieldType))
 }
 
-func (p *ValuesParser) parseModel(value interface{}, model scheme.Model) (interface{}, error) {
-	var fieldsMap map[string]interface{}
-	var ok bool
-	if fieldsMap, ok = value.(map[string]interface{}); !ok {
-		return nil, errors.ParsingError.Newf("model %s must be a map", model.Name)
-	}
+func (p *ValuesParser) parseModel(value map[string]interface{}, model scheme.Model) map[string]interface{} {
+	fieldsMap := value
 	modelFieldNames := make(map[string]int)
 	for i, field := range model.Fields {
 		modelFieldNames[field.Name] = i
 	}
 	for name, value := range fieldsMap {
 		if _, ok := modelFieldNames[name]; !ok {
-			return nil, errors.ParsingError.Newf("model %s: field %s is not defined", model.Name, name)
+			panic(errors.ParsingError.Newf("model %s: field %s is not defined", model.Name, name))
 		}
-		value, err := p.parseField(value, model.Fields[modelFieldNames[name]].Type)
-		if err != nil {
-			return nil, err
-		}
+		value := p.parseField(value, model.Fields[modelFieldNames[name]].Type)
 		fieldsMap[name] = value
 	}
-	return fieldsMap, nil
+	return fieldsMap
 }
 
-func (p *ValuesParser) parseModels(models []scheme.Model) error {
-	var err error
+func (p *ValuesParser) parseModels(models []scheme.Model) {
 	for _, model := range models {
 		for _, field := range model.Fields {
-			if field.DefaultValue != nil {
-				if field.DefaultValue, err = p.parseField(field.DefaultValue, field.Type); err != nil {
-					return err
-				}
-			}
+			field.DefaultValue = p.parseField(field.DefaultValue, field.Type)
 		}
 	}
-	return nil
 }
 
-func (p *ValuesParser) parseTables(tables []scheme.Table) error {
-	var err error
+func (p *ValuesParser) parseTables(tables []scheme.Table) {
 	for _, table := range tables {
 		for _, column := range table.Columns {
-			if column.DefaultValue != nil {
-				if column.DefaultValue, err = p.parseField(column.DefaultValue, column.Type); err != nil {
-					return err
-				}
-			}
+			column.DefaultValue = p.parseField(column.DefaultValue, column.Type)
 		}
 	}
-	return nil
 }
 
-func (p *ValuesParser) ParseValues(config *scheme.GeneratorConfig) error {
-	if err := p.parseModels(config.Models); err != nil {
-		return err
-	}
-	return p.parseTables(config.Tables)
+func (p *ValuesParser) ParseValues(config *scheme.GeneratorConfig) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if e, ok := r.(error); ok {
+				err = e
+			} else {
+				err = errors.InternalError.Newf("%v", r)
+			}
+		}
+	}()
+	p.parseModels(config.Models)
+	p.parseTables(config.Tables)
+	return err
 }
 
 func NewValuesParser(models []scheme.Model) (*ValuesParser, error) {
