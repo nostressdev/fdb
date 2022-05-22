a7fcfb7dfcb562ad5917fc801b760571b0cfaeee
lev <wot.k@mail.ru>
2022-05-22 22:01:11 +0000

    refactor errors tests

--- errors/errors_test.go
+++ errors/errors_test.go
@@ -8,55 +8,111 @@ import (
 
 func TestErrorWrapping(t *testing.T) {
 	err := errors.New("error")
-	newErr := ParsingError.Wrap(err, "wrapped error")
-	if newErr.Error() != "parsing error: wrapped error: error" {
-		t.Fatalf("error wrapping error: got %q, want %q", newErr.Error(), "wrapped error: error")
+	tests := []struct {
+		name string
+		args ErrorType
+	}{
+		{
+			name: "parsing error",
+			args: ParsingError,
+		},
+		{
+			name: "validation error",
+			args: ValidationError,
+		},
+		{
+			name: "internal error",
+			args: InternalError,
+		},
+		{
+			name: "no type error",
+			args: NoType,
+		},
 	}
-	oldErr := Unwrap(err)
-	if oldErr != err {
-		t.Fatalf("error unwrapping error: got %q, want %q", oldErr.Error(), err.Error())
-	}
-}
-
-func TestAddedMessage(t *testing.T) {
-	err := ParsingError.New("error")
-	if err.Error() != "parsing error: error" {
-		t.Fatalf("error adding message: got %q, want %q", err.Error(), "parsing error: error")
-	}
-	err = ValidationError.New("error")
-	if err.Error() != "validation error: error" {
-		t.Fatalf("error adding message: got %q, want %q", err.Error(), "validation error: error")
-	}
-	err = InternalError.New("error")
-	if err.Error() != "internal error: error (please report this error)" {
-		t.Fatalf("error adding message: got %q, want %q", err.Error(), "internal error: error")
-	}
-	err = NoType.New("error")
-	if err.Error() != "error" {
-		t.Fatalf("error adding message: got %q, want %q", err.Error(), "error")
-	}
-}
-
-func TestErrorType(t *testing.T) {
-	err := ParsingError.New("error")
-	if GetType(err) != ParsingError {
-		t.Fatalf("error type: got %q, want %q", GetType(err), ParsingError)
-	}
-	err = ValidationError.New("error")
-	if GetType(err) != ValidationError {
-		t.Fatalf("error type: got %q, want %q", GetType(err), ValidationError)
-	}
-	err = InternalError.New("error")
-	if GetType(err) != InternalError {
-		t.Fatalf("error type: got %q, want %q", GetType(err), InternalError)
-	}
-	err = NoType.New("error")
-	if GetType(err) != NoType {
-		t.Fatalf("error type: got %q, want %q", GetType(err), NoType)
-	}
-	err = errors.New("error")
-	if GetType(err) != NoType {
-		t.Fatalf("error type: got %q, want %q", GetType(err), NoType)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			newErr := tt.args.Wrap(err, "wrapped error")
+			oldErr := Unwrap(newErr)
+			if oldErr.Error() != "wrapped error: " + err.Error() {
+				t.Fatalf("error unwrapping error: got %q, want %q", oldErr.Error(), err.Error())
+			}
+		})
 	}
 }
 
+func TestGetType(t *testing.T) {
+	tests := []struct {
+		name string
+		args error
+		want ErrorType
+	}{
+		{
+			name: "parsing error",
+			args: ParsingError.New("error"),
+			want: ParsingError,
+		},
+		{
+			name: "validation error",
+			args: ValidationError.New("error"),
+			want: ValidationError,
+		},
+		{
+			name: "internal error",
+			args: InternalError.New("error"),
+			want: InternalError,
+		},
+		{
+			name: "explicit no type error",
+			args: NoType.New("error"),
+			want: NoType,
+		},
+		{
+			name: "implicit no type error",
+			args: errors.New("error"),
+			want: NoType,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetType(tt.args); got != tt.want {
+				t.Errorf("GetType() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestErrorMessage(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    error
+		wantErr string
+	}{
+		{
+			name:    "parsing error",
+			args:    ParsingError.New("error"),
+			wantErr: "parsing error: error",
+		},
+		{
+			name:    "validation error",
+			args:    ValidationError.New("error"),
+			wantErr: "validation error: error",
+		},
+		{
+			name:    "internal error",
+			args:    InternalError.New("error"),
+			wantErr: "internal error: error (please report this error)",
+		},
+		{
+			name:    "no type error",
+			args:    NoType.New("error"),
+			wantErr: "error",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.args.Error() != tt.wantErr {
+				t.Errorf("Unwrap() error = %v, wantErr %v", tt.args.Error(), tt.wantErr)
+			}
+		})
+	}
+}
