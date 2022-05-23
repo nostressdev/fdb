bc8bba1af588d188574c1f992b49c2b2c410bb03
MKuchum <s19b1_kuchumov@179.ru>
2022-05-23 08:22:30 +0300

    change asserts

--- Makefile
+++ Makefile
@@ -1,13 +1,3 @@
-SRC := $(shell find . -name "*.go")
-
-all: bin/chat
-
-bin/chat: cmd/main.go $(SRC) pb
-	go build -o bin/chat cmd/main.go
-
-run: bin/chat
-	./bin/chat
-
 test:
 	go test -v ./tests -run Test_Generate
 	go test -v ./tests -run Test_CreateTable
@@ -17,6 +7,3 @@ test_with_env:
 	docker-compose -f docker-compose.test.yaml up -d fdb
 	docker-compose -f docker-compose.test.yaml run fdb-service
 	docker-compose -f docker-compose.test.yaml down
-
-run_with_env:
-	docker-compose up
--- tests/foundation_test.go
+++ tests/foundation_test.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"github.com/apple/foundationdb/bindings/go/src/fdb"
 	gen_fdb "github.com/nostressdev/fdb/tests/generated"
+	"github.com/stretchr/testify/assert"
 	"log"
 	"testing"
 )
 
 func Test_CreateTable(t *testing.T) {
 	table, err := gen_fdb.NewUsersTable(toUsers)
-	AssertError(t, err)
+	assert.Nil(t, err)
 	row := &gen_fdb.UsersTableRow{Man: gen_fdb.User{ID: "id", Age: 57}, Ts: 123}
 	fdb.MustAPIVersion(600)
 	db := fdb.MustOpenDefault()
@@ -24,21 +25,21 @@ func Test_CreateTable(t *testing.T) {
 		return nil, err
 	})
 	log.Println("2")
-	AssertError(t, err)
+	assert.Nil(t, err)
 
 	log.Println("3")
 
 	future, err := db.ReadTransact(func(tr fdb.ReadTransaction) (interface{}, error) {
 		return table.Get(tr, &gen_fdb.UsersTablePK{Ts: row.Ts, ManID: row.Man.ID})
 	})
-	AssertError(t, err)
+	assert.Nil(t, err)
 	if future == nil {
-		AssertError(t, fmt.Errorf("future is nil"))
+		assert.Nil(t, fmt.Errorf("future is nil"))
 	}
 
 	log.Println("4")
 
 	resRow, err := future.(*gen_fdb.FutureUsersTableRow).Get()
-	AssertError(t, err)
-	AssertEqual(t, row, resRow, "equal res")
+	assert.Nil(t, err)
+	assert.Equal(t, row, resRow, "equal res")
 }
